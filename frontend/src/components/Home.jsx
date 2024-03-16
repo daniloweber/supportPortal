e198a60648dd02aaf2fec97ca33b1bba98eaeb36
markusszr <markusschwarzlaender@gmail.com>
2024-03-16 20:25:49 +0100

    Adi fix

---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Willkommen auf der Homepage</h1>
+            <p>Hier können Sie Informationen über unsere Website finden.</p>
+        </div>
+    );
+}
+
+export default Home;
---
+++ frontend/src/components/TicketView.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class TicketView extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Ticket View</h1>
+                {/* You can add more components or HTML elements here */}
+            </div>
+        );
+    }
+}
+
+export default TicketView;
